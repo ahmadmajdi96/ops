70049062959cb45338b81c8daa470d7178fbce88
ahmad <ahmad.majdi96@gmail.com>
2025-09-22 17:39:37 +0300

    request update

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ class Handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b"Hello from myapp!\n")
+        print("done")
 
 if __name__ == "__main__":
     HTTPServer(("0.0.0.0", 8080), Handler).serve_forever()
